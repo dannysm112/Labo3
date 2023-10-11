db48dd3969d695574a2d7342b3c2160b4569eb81
dannysm112 <dannysolorzano71@gmail.com>
2023-10-11 01:13:37 -0600

    Arreglos y punteros

---
+++ Parte2.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+int encontrarMaximo(int arreglo[], int longitud){
+	int max = arreglo[0];
+	for(int i=1;i<longitud;i++){
+		if(arreglo[i]> max){
+			max = arreglo[i];
+		}
+	}include <stdio.h>
+
+int encontrarMaximo(int arreglo[], int longitud){
+        int max = arreglo[0];
+        for(int i=1;i<longitud;i++){
+                if(arreglo[i]> max){
+                        max = arreglo[i];
+                }
+        }
+        return max;
+}
+
+int encontrarMinimo(int arreglo[], int longitud){
+        int min = arreglo[0];
+        for(int i=1;i<longitud;i++){
+                if(arreglo[i]<min){
+                        min = arreglo[i];
+                }
+        }
+        return min;
+}
+
+	return max;
+}
+
+int encontrarMinimo(int arreglo[], int longitud){
+	int min = arreglo[0];
+	for(int i=1;i<longitud;i++){
+		if(arreglo[i]<min){
+			min = arreglo[i];
+		}
+	}
+	return min;
+}
+
+void encontrarMaxMin(int *arreglo, int *min, int *max, int longitud){
+	*min = *arreglo;
+	*max = *arreglo;
+	for(int i=1;i<longitud;i++){
+		if(*(arreglo+i) > *max){
+			*max = *(arreglo+i);
+		}
+	}
+	
+        for(int i=1;i<longitud;i++){
+                if(*(arreglo+i) < *min){
+                        *min = *(arreglo+i);
+                }
+        }
+
+}
+
+int main(){
+
+	int arreglo[] = {21,24,65,3,56,12,35,15};
+	int longitud = sizeof(arreglo)/sizeof(arreglo[0]);
+	int maximo = encontrarMaximo(arreglo, longitud);
+	printf("El máximo con la función encontrarMáximo es: %d\n", maximo);
+
+	int minimo = encontrarMinimo(arreglo, longitud);
+        printf("El mínimo con la función encontrarMínimo es: %d\n", minimo);
+
+	int max;
+	int min;
+	encontrarMaxMin(arreglo, &min, &max, longitud);
+	
+	printf("El máximo y mínimo con la función encontrarMaxMin son: %d y %d\n", max, min); 
+
+	return 0;
+}
